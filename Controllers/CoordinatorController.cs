d79ac77a567307c7adc2511fd8f4d44ad78ec194
makom <makom@LAPTOP-3R33KP50>
2025-11-19 17:40:39 +0200

    added the using CMCS.Model in the Coordinator and Manager controller.

--- Controllers/CoordinatorController.cs
+++ Controllers/CoordinatorController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using CMCS.Data;
+using CMCS.Models; 
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Linq;
--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using CMCS.Data;
+using CMCS.Models; 
 using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
